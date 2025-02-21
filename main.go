de3db1ddbd80068f8f0d637ba4825afbfbba01cc
RITUPARNA MANDAL <32814143+m-rit@users.noreply.github.com>
2025-02-20 22:13:09 -0600

    Update main.go

--- main.go
+++ main.go
@@ -7,19 +7,16 @@ import (
 	"log"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
-// are they supposed to be public?
 
 func main() {
 
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
+	
 	ctx := context.Background()
 
 	log.Println("initializing application")
+	\\initializes sqlite tables
 	pers.InitDB()
+	\\ registers handlers for scan and query and starts server
 	middleware.Inithandlers(ctx)
 
 }
